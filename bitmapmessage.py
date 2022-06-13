16ffc6e31cb735ebce50a98058ee63506c6256bd
Daniel Joseph T. Castro <90781857+dtjunkie@users.noreply.github.com>
2022-06-13 11:50:11 +0800

    Project 3: Bitmap Message

---
+++ bitmapmessage.py
@@ -0,0 +1,52 @@
+"""Bitmap Message, by Al Sweigart al@inventwithpython.com
+Displays a text message according to the provided bitmap image.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: tiny, beginner, artistic"""
+
+import sys
+
+
+
+
+
+
+bitmap = """
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+...................................................................."""
+
+print('Bitmap Message, by Al Sweigart al@inventwithpython.com')
+print('Enter the message to display with the bitmap.')
+message = input('> ')
+if message == '':
+    sys.exit()
+
+
+for line in bitmap.splitlines():
+
+    for i, bit in enumerate(line):
+        if bit == ' ':
+
+            print(' ',end = '')
+        else:
+
+            print(message[i % len(message)], end ='')
+    print()
